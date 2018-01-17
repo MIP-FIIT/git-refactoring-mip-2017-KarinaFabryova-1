ca80858d46548568f6c1a7d41c65e80e6df6df3d
slavejko <s.slavejko@gmail.com>
2018-01-17 13:44:45 +0100

    upreva4

--- unit1.pas
+++ unit1.pas
@@ -36,7 +36,7 @@ var
   i: integer;
   j: integer;
   s: integer;
-  max: integer;
+  m: integer;
   t: text;
   riadok: string;
 
@@ -61,20 +61,20 @@ begin
         begin
           riadok:=ziaci[i].name;
           s:=0;
-          max:=ziaci[i].znamky[2];
+          m:=ziaci[i].znamky[2];
           for j:=2 to 7 do
                 begin
                   s:=s+ziaci[i].znamky[j];
-                  if ziaci[i].znamky[j]>max then max:=ziaci[i].znamky[j];
+                  if ziaci[i].znamky[j]>m then m:=ziaci[i].znamky[j];
                   riadok:=riadok+' '+IntToStr(ziaci[i].znamky[j]);
                 end;
           ziaci[i].priemer:=s/6;
           riadok:=riadok+' priemer: '+FloatToStr(ziaci[i].priemer);
-          if (ziaci[i].znamky[1]=1) and (ziaci[i].priemer<=1.5) and (max<3)
+          if (ziaci[i].znamky[1]=1) and (ziaci[i].priemer<=1.5) and (m<3)
              then ziaci[i].prospech:='PV'
-             else if (ziaci[i].znamky[1]=1) and (ziaci[i].priemer<=2) and (max<4)
+             else if (ziaci[i].znamky[1]=1) and (ziaci[i].priemer<=2) and (m<4)
                 then ziaci[i].prospech:='PVD'
-                else if max<5 then ziaci[i].prospech:='P'
+                else if m<5 then ziaci[i].prospech:='P'
                               else ziaci[i].prospech:='N';
           riadok:=riadok+' prospech: '+ziaci[i].prospech;
           Memo1.Lines.Add(riadok);
